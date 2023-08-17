b2ce7e7bfcd082653ca74cb6479064f035c2ba64
G0vinda <frederickfreund@gmail.com>
2023-08-17 15:26:21 +0200

    Make menu button invisible in programView

--- Assets/Scripts/UI/Buttons/MenuButton.cs
+++ Assets/Scripts/UI/Buttons/MenuButton.cs
@@ -1,17 +1,48 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI.Buttons
 {
     public class MenuButton : MonoBehaviour
     {
-        [SerializeField] private int MenuSceneIndex;
-    
+        [SerializeField] private int menuSceneIndex;
+
+        private Button _button;
+        private Image _image;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _image = GetComponent<Image>();
+        }
+
+        #region OnEnable/OnDisable
+
+        private void OnEnable()
+        {
+            UIManager.ViewStateChanged += OnViewStateChanged;
+        }
+
+        private void OnDisable()
+        {
+            UIManager.ViewStateChanged -= OnViewStateChanged;
+        }
+
+        #endregion
+
         public void ReturnToMenu()
         {
             SoundPlayer.Instance.PlayMusicMenu();
             SoundPlayer.Instance.StopAtmoLevel();
-            SceneManager.LoadScene(MenuSceneIndex);
+            SceneManager.LoadScene(menuSceneIndex);
+        }
+
+        private void OnViewStateChanged(bool programmingViewActive)
+        {
+            _button.interactable = !programmingViewActive;
+            _image.enabled = !programmingViewActive;
         }
     }
 }
